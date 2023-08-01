6409cf2c60766c320f353330c8abb0685ce86bed
Abigail Peloquin <abigail.turnwall@gmail.com>
2023-08-01 17:36:09 -0500

    cleaned up styling and worked on the modal

--- bulma.js
+++ bulma.js
@@ -1,3 +1,37 @@
+//this will trigger the modal
+const modalEl = document.querySelector(".modal-element");
+
+document.addEventListener("click", function (event) {
+  const target = event.target;
+  // if the modal button is clicked, then open the modal
+  if (target.classList.contains("modal-button")) {
+    openModal();
+  }
+
+  // if the submit or close buttons are clicked, then close the modal
+  if (
+    target.classList.contains("submit-button") ||
+    target.classList.contains("modal-close")
+  ) {
+    closeModal();
+  }
+
+  // if a user clicks outside of the modal, then close the modal
+  if (!modalEl.contains(target)) {
+    closeModal();
+  }
+});
+
+function openModal() {
+  const modalContainer = document.querySelector(".modal");
+  modalContainer.classList.add("is-active");
+}
+
+function closeModal() {
+  modalEl.classList.remove("is-active");
+}
+
+// this will trigger the dropdowns
 document.addEventListener("DOMContentLoaded", function () {
   const options = document.querySelectorAll(".dropdown");
   if (options.length > 0) {
@@ -12,7 +46,7 @@ document.addEventListener("DOMContentLoaded", function () {
       dropdownTrigger.addEventListener("click", function (event) {
         event.stopPropagation();
         closeAllDropdowns();
-        
+
         const isActive = dropdown.classList.contains("is-active");
         if (!isActive) {
           dropdown.classList.add("is-active");
